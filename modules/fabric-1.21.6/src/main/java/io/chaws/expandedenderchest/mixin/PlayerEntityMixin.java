41c5ecf88c1b8c3a7bc264ef86ff3d6ca4f94ed8
Chris Haws <chris@hawslabs.com>
2025-07-20 13:21:13 -0700

    fix mixin to work with 1.21.6

--- modules/fabric-1.21.6/src/main/java/io/chaws/expandedenderchest/mixin/PlayerEntityMixin.java
+++ modules/fabric-1.21.6/src/main/java/io/chaws/expandedenderchest/mixin/PlayerEntityMixin.java
@@ -3,6 +3,7 @@ package io.chaws.expandedenderchest.mixin;
 import com.mojang.authlib.GameProfile;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -11,11 +12,11 @@ import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.inventory.EnderChestInventory;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.collection.DefaultedList;
-import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 
 @Mixin(PlayerEntity.class)
 public abstract class PlayerEntityMixin {
+	@Unique
 	private static final int EXPANDED_ENDERCHEST_SIZE = 9 * 6;
 
 	@Shadow
@@ -23,11 +24,9 @@ public abstract class PlayerEntityMixin {
 
 	@Inject(method = "<init>", at = @At(value = "RETURN"))
 	private void expandEnderChest(
-		final World world,
-		final BlockPos pos,
-		final float yaw,
-		final GameProfile gameProfile,
-		final CallbackInfo ci
+		World world,
+		GameProfile profile,
+		CallbackInfo ci
 	) {
 		var accessor = (SimpleInventoryAccessor)getEnderChestInventory();
 		accessor.setSize(EXPANDED_ENDERCHEST_SIZE);
