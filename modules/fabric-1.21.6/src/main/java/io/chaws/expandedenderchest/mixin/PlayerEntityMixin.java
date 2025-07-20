5e582b3cab2406fdf4134f20d767ca446df81f2a
Christopher Haws <cribs2@gmail.com>
2025-07-20 13:37:00 -0700

    Add support for mc1.21.6 (#86)
    * add support for 1.21.6
    * fix mixin to work with 1.21.6
    * update gradlew to 8.14.3, loom to 1.11, and jetbrains plugin to 1.2
    
    ---------
    
    Co-authored-by: Chris Haws <chris@hawslabs.com>

---
+++ modules/fabric-1.21.6/src/main/java/io/chaws/expandedenderchest/mixin/PlayerEntityMixin.java
@@ -0,0 +1,35 @@
+package io.chaws.expandedenderchest.mixin;
+
+import com.mojang.authlib.GameProfile;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.inventory.EnderChestInventory;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.collection.DefaultedList;
+import net.minecraft.world.World;
+
+@Mixin(PlayerEntity.class)
+public abstract class PlayerEntityMixin {
+	@Unique
+	private static final int EXPANDED_ENDERCHEST_SIZE = 9 * 6;
+
+	@Shadow
+	public abstract EnderChestInventory getEnderChestInventory();
+
+	@Inject(method = "<init>", at = @At(value = "RETURN"))
+	private void expandEnderChest(
+		World world,
+		GameProfile profile,
+		CallbackInfo ci
+	) {
+		var accessor = (SimpleInventoryAccessor)getEnderChestInventory();
+		accessor.setSize(EXPANDED_ENDERCHEST_SIZE);
+		accessor.setStacks(DefaultedList.ofSize(EXPANDED_ENDERCHEST_SIZE, ItemStack.EMPTY));
+	}
+}
